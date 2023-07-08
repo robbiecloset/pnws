0d5e06c37474660d7602dd33b8eff3a126f0742e
robbiecloset <robbiecloset@gmail.com>
2023-07-08 08:31:23 -0400

    i get it i get it

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +11,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="flex min-h-screen flex-col p-24">
+          <h1>practice noticing & writing sentences</h1>
+          <ul>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+          </ul>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
-      <h1>practice noticing & writing sentences</h1>
-    </main>
+    <h2>Home page title content</h2>
   )
 }
