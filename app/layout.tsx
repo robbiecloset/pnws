d488269bcb7addcedd144301193243fe9b063b34
John Corrigan <john.corrigan@donut.ai>
2023-07-26 15:37:36 -0400

    WIP on aside menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,28 @@ export default function RootLayout({
       <body className={inter.className}>
         <main className="flex min-h-screen flex-col p-24">
           <h1>practice noticing & writing sentences</h1>
-          <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/about">About</Link></li>
-          </ul>
+
+          <aside className="fixed top-0 left-0 w-64 h-full" aria-label="Sidenav">
+            <div className="overflow-y-auto py-5 px-3 h-full bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+              <ul className="space-y-2">
+                <li>
+                  <Link
+                    className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                    href="/">
+                      Home
+                    </Link>
+                </li>
+                <li>
+                  <Link
+                    className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                    href="/about">
+                      About
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </aside>
+
           {children}
         </main>
       </body>
